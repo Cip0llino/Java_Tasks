2db1786af2f963760c40c70814aadfe0f7c1a7b3
Cipollino <cipollino.email@gmail.com>
2021-11-30 01:32:06 +0300

    Високосный и невисокосный год

--- src/ru/geegbrains/Test.java
+++ src/ru/geegbrains/Test.java
@@ -1,7 +1,29 @@
 package ru.geegbrains;
 
+import java.util.Scanner;
+
 public class Test {
+    public static final double PI = 3.14;
+
+    public static double calculateCircleArea(int radius) {
+        return PI * radius * radius;
+    }
+
+    public static double calculateCircleLength(int radius) {
+        return 2 * PI * radius;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите радиус окружности в сантиметрах:");
+        int radius = scanner.nextInt();
+
+        double area = calculateCircleArea(radius);
+        double length = calculateCircleLength(radius);
+
+        System.out.println("Площадь круга: " + area +  " см.кв.");
+        System.out.println("Длина окружности: " + length +  " см.");
+
     }
 }
---
+++ src/ru/tasks/year/Main.java
@@ -0,0 +1,34 @@
+package ru.tasks.year;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        int leapYear = 366;
+        int notLeapYear = 365;
+        int resultYear;
+
+        while (true) {
+            System.out.println("Введите год и количество дней в формате yyyy число");
+            int year = scanner.nextInt();
+            int numder = scanner.nextInt();
+            boolean resultCheckLeapYear = checkLeapYear(year);
+
+            if (resultCheckLeapYear && numder == leapYear || !resultCheckLeapYear && numder == notLeapYear){
+                count++;
+            } else {
+                resultYear = resultCheckLeapYear ? leapYear : notLeapYear;
+                System.out.println("Неправильно! В этом году " + resultYear +" дней!");
+                System.out.println("Набрано очков: " + count);
+                break;
+            }
+        }
+    }
+
+    public static boolean checkLeapYear(int year) {
+        return year % 400 == 0 && year % 100 != 0 || year % 4 == 0;
+    }
+}
